f9829f8d0a038cb98eb510fac0891e4938dae727
Beka Aptsiauri <bekatiflis@gmail.com>
2016-08-03 00:25:34 +0200

    LookRight

---
+++ src/de/dfki/stickmanfx/animation/headfx/LookRight.java
@@ -0,0 +1,39 @@
+package de.dfki.stickmanfx.animation.headfx;
+
+import de.dfki.stickman.Stickman;
+import de.dfki.stickman.animationlogic.Animation;
+import de.dfki.stickman.animationlogic.AnimationContent;
+import de.dfki.stickmanfx.StickmanFX;
+import de.dfki.stickmanfx.animationlogic.AnimationContentFX;
+import de.dfki.stickmanfx.animationlogic.AnimationFX;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Patrick Gebhard
+ *
+ */
+public class LookRight extends AnimationFX {
+
+	public LookRight(StickmanFX sm, int duration, boolean block) {
+		super(sm, duration, block);
+	}
+
+	@Override
+	public void playAnimation() {
+		int translationUnit = 3;
+
+		// look left
+		mAnimationPartFX = new ArrayList<>();
+		mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mLeftEyeFX, "shape", "LOOKRIGHT"));
+		mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mRightEyeFX, "shape", "LOOKRIGHT"));
+		playAnimationPart(20);
+
+//		// blink up
+//		mAnimationPart = new ArrayList<>();
+//		mAnimationPart.add(new AnimationContent(mStickman.mLeftEye, "shape", "DEFAULT"));
+//		mAnimationPart.add(new AnimationContent(mStickman.mRightEye, "shape", "DEFAULT"));
+//		playAnimationPart(2);
+	}
+}
