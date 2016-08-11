f4b8cd2d4817a831c60842bd3a720034b545259c
Beka Aptsiauri <bekatiflis@gmail.com>
2016-08-11 14:10:01 +0200

    RightLeg. change Color.rgb to new Color

--- src/de/dfki/stickmanfx/bodyfx/RightForeLegFX.java
+++ src/de/dfki/stickmanfx/bodyfx/RightForeLegFX.java
@@ -95,7 +95,8 @@ public class RightForeLegFX extends BodyPartFX {
                 {
                     fadeFactor = 0;
                 }
-                mColor = Color.rgb(80, 80, 80, (fadeFactor*100/255)/100f);
+                mColor = new Color(mColor.getRed(), mColor.getGreen(), mColor.getBlue(), (fadeFactor * 100 / 255) / 100f);
+                //mColor = Color.rgb(80, 80, 80, (fadeFactor*100/255)/100f);
             } 
             else 
             {
@@ -104,7 +105,8 @@ public class RightForeLegFX extends BodyPartFX {
                 {
                     fadeFactor = 255;
                 }
-                mColor = Color.rgb(80, 80, 80, (fadeFactor*100/255)/100f);
+                mColor = new Color(mColor.getRed(), mColor.getGreen(), mColor.getBlue(), (fadeFactor * 100 / 255) / 100f);
+                //mColor = Color.rgb(80, 80, 80, (fadeFactor*100/255)/100f);
             }
         }
     	
