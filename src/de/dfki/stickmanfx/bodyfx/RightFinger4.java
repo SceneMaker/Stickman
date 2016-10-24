13051e3f98f3b1a5d6117560fc04399c8d4618e8
Beka Aptsiauri <bekatiflis@gmail.com>
2016-10-25 01:30:54 +0200

    Finger dick

--- src/de/dfki/stickmanfx/bodyfx/RightFinger4.java
+++ src/de/dfki/stickmanfx/bodyfx/RightFinger4.java
@@ -52,7 +52,7 @@ public class RightFinger4 extends BodyPartFX
     	imorter = new ColModelImporter();
         mColor = Color.rgb(242, 227, 217, 1);
         
-        url = getClass().getClassLoader().getResource("BodyParts/Finger2_3_4.dae");
+        url = getClass().getClassLoader().getResource("BodyParts/Finger2_3_4_2.dae");
         
         mDefaultRotation = -20;
         mZRotation = 0;
