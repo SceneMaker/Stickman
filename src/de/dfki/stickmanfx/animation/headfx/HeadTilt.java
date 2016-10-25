c92bd832ab81373938bd4bc771897d4c9d8739ed
Beka Aptsiauri <bekatiflis@gmail.com>
2016-10-26 00:38:11 +0200

    HeadTilt

--- src/de/dfki/stickmanfx/animation/headfx/HeadTilt.java
+++ src/de/dfki/stickmanfx/animation/headfx/HeadTilt.java
@@ -6,8 +6,11 @@
 package de.dfki.stickmanfx.animation.headfx;
 
 import de.dfki.stickmanfx.StickmanFX;
+import de.dfki.stickmanfx.StickmanStageController;
 import de.dfki.stickmanfx.animationlogic.AnimationContentFX;
 import de.dfki.stickmanfx.animationlogic.AnimationFX;
+import de.dfki.stickmanfx.animationlogic.AnimationFX.ANIMTYPE;
+
 import java.util.ArrayList;
 import javafx.application.Platform;
 
@@ -18,7 +21,9 @@ import javafx.application.Platform;
  */
 public class HeadTilt extends AnimationFX 
 {
-	
+	public HeadTilt() {
+		mAnimType = ANIMTYPE.ON;
+	}
 
     public HeadTilt(StickmanFX sm, int duration, boolean block) 
     {
@@ -27,42 +32,14 @@ public class HeadTilt extends AnimationFX
 
     @Override
     public void playAnimation() {
-        int translationUnit = 8;
-
-        // head down
-        mAnimationPartFX = new ArrayList<>();
-        // which bodyparts are involved - check dependencies
-        mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mRightEyeFX, "tilt", translationUnit));
-        mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mRightEyebrowFX, "tilt", translationUnit));
-        mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mLeftEyeFX, "tilt", translationUnit));
-        mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mLeftEyebrowFX, "tilt", translationUnit));
-        mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mHeadFX, "tilt", translationUnit));
-        
-        if(mStickmanFX.mType == StickmanFX.TYPE.MALE)
-        	mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mMaleHairFX, "tilt", translationUnit));
-        else
-        	mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mFemaleHairFX, "tilt", translationUnit));
-        
-        mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mMouthFX, "tilt", translationUnit));
-
-        playAnimationPart(150);
-
-        pauseAnimation(200);
-
-        mAnimationPartFX = new ArrayList<>();
-        // which bodyparts are involved - check dependencies
-        mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mRightEyeFX, "tilt", -translationUnit));
-        mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mRightEyebrowFX, "tilt", -translationUnit));
-        mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mLeftEyeFX, "tilt", -translationUnit));
-        mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mLeftEyebrowFX, "tilt", -translationUnit));
-        mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mHeadFX, "tilt", -translationUnit));
-        
-        if(mStickmanFX.mType == StickmanFX.TYPE.MALE)
-        	mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mMaleHairFX, "tilt", -translationUnit));
-        else
-        	mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mFemaleHairFX, "tilt", -translationUnit));
-        
-        mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mMouthFX, "tilt", -translationUnit));
-        playAnimationPart(150);
+    	
+    	mAnimationPartFX = new ArrayList<>();
+		mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mHeadFX, "zrotate", -10));
+		playAnimationPart(200);
+		
+		mAnimationPartFX = new ArrayList<>();
+		mAnimationPartFX.add(new AnimationContentFX(mStickmanFX.mHeadFX, "zrotate", 10));
+		playAnimationPart(200);
+    	StickmanStageController.currentRadioButton.setSelected(false);
     }
 }
