6005dc7d401a823a231957ed3818f9db88f5e2a8
Beka Aptsiauri <bekatiflis@gmail.com>
2016-10-30 03:21:59 +0100

    RightEye

--- src/de/dfki/stickmanfx/bodyfx/RightEyeFX.java
+++ src/de/dfki/stickmanfx/bodyfx/RightEyeFX.java
@@ -69,7 +69,34 @@ public class RightEyeFX extends BodyPartFX {
 
 		smallPupileColor = Color.rgb(255, 255, 255, 1);
 		borderColor = Color.rgb(255, 255, 255, 1);
+		
+		mStart = mHeadFX.getRightEyePostion();
+		
+		border = new Path();
+		bigPupile = new Path();
+		smallPupile = new Path();
+		
+		rightEyeGroup = new Group();
+		rightEyeGroup.getChildren().add(border);
+		rightEyeGroup.getChildren().add(bigPupile);
+		rightEyeGroup.getChildren().add(smallPupile);
+		mHeadFX.mHead.getChildren().add(rightEyeGroup);
+		
 		init();
+	}
+	
+	@Override
+	public void init()
+	{
+		super.init();
+		bigPupile.setTranslateX(mStart.x - 7);
+		bigPupile.setTranslateY(mStart.y);
+		smallPupile.setTranslateX(mStart.x - 9);
+		smallPupile.setTranslateY(mStart.y);
+		border.setTranslateZ(-17);
+		bigPupile.setTranslateZ(-18);
+		smallPupile.setTranslateZ(-19);
+
 	}
 
 	@Override
@@ -85,61 +112,23 @@ public class RightEyeFX extends BodyPartFX {
 
 	@Override
 	public void calculate(int step) {
-		mStart = mHeadFX.getRightEyePostion();
 		
 		float xMovement;
 		float yMovement;
 		
 		switch (mShape) {
 		case DEFAULT:
-			if (mHeadFX.mStickmanFX.setCharacterInvisible == true) {
-				if (mHeadFX.mStickmanFX.fadeControler == true) 
-				{
-					int fadeFactor = mHeadFX.mStickmanFX.mMouthFX.mShapeAnimationStep * 7;
-					if (fadeFactor <= 14) {
-						fadeFactor = 0;
-					}
-					mColor = Color.rgb(mHeadFX.mStickmanFX.mType == StickmanFX.TYPE.FEMALE ? 22 : 0,
-							mHeadFX.mStickmanFX.mType == StickmanFX.TYPE.FEMALE ? 40 : 0,
-							mHeadFX.mStickmanFX.mType == StickmanFX.TYPE.FEMALE ? 65 : 0,
-							(fadeFactor * 100 / 255) / 100f);
-				} else {
-					int fadeFactor = (20 - mHeadFX.mStickmanFX.mMouthFX.mShapeAnimationStep) * 7;
-
-					if (fadeFactor >= 119) {
-						mColor = mColorRecorder;
-					} else
-						mColor = Color.rgb(mHeadFX.mStickmanFX.mType == StickmanFX.TYPE.FEMALE ? 22 : 0,
-								mHeadFX.mStickmanFX.mType == StickmanFX.TYPE.FEMALE ? 40 : 0,
-								mHeadFX.mStickmanFX.mType == StickmanFX.TYPE.FEMALE ? 65 : 0,
-								(fadeFactor * 100 / 255) / 100f);
-				}
+			if(step == 20 || step == 0)
+			{
+				border = createBorder(border);
+				bigPupile = createEllipsePath(bigPupile, 0, 0, 3.5, 3.5, 0, mColor, null);
+				smallPupile = createEllipsePath(smallPupile, 0, 0, 1.4, 1.4, 0, Color.WHITE, null);
+				smallPupile.setStroke(null);
+				borderYSize = 0;
+				borderXSize = 0;
+				bigPupileYSize = 0;
+				smallPupileYSize = 0;
 			}
-			
-			border = new Path();
-			border.getElements().add(new MoveTo(mStart.x, mStart.y));
-			border.getElements().add(new QuadCurveTo(mStart.x - 10, mStart.y - 13, mStart.x - 20, mStart.y));
-			border.getElements().add(new QuadCurveTo(mStart.x - 10, mStart.y + 13, mStart.x, mStart.y));
-			border.setStrokeWidth(1);
-			border.setStroke(Color.BLACK);
-			border.setFill(Color.WHITE);
-			
-			bigPupile = createEllipsePath(0, 0, 3.5, 3.5, 0, mColor, null);
-			
-			smallPupile = createEllipsePath(0, 0, 1.4, 1.4, 0, Color.WHITE, null);
-			smallPupile.setStroke(null);
-			
-			bigPupile.setTranslateX(mStart.x - 7);
-			bigPupile.setTranslateY(mStart.y);
-			
-			smallPupile.setTranslateX(mStart.x - 9);
-			smallPupile.setTranslateY(mStart.y);
-			
-			borderYSize = 0;
-			borderXSize = 0;
-			bigPupileYSize = 0;
-			smallPupileYSize = 0;
-
 			break;
 			
 		case FADEIN:
@@ -364,21 +353,6 @@ public class RightEyeFX extends BodyPartFX {
 			smallPupile.setTranslateY(smallPupile.getTranslateY() + yMovement);
 			break;
 		}
-		
-		border.setTranslateZ(-17);
-		bigPupile.setTranslateZ(-18);
-		smallPupile.setTranslateZ(-19);
-		
-		rightEyeGroup = new Group();
-		rightEyeGroup.setId("RightEye");
-		rightEyeGroup.getChildren().add(border);
-		rightEyeGroup.getChildren().add(bigPupile);
-		rightEyeGroup.getChildren().add(smallPupile);
-		
-		if (step == 0) 
-			mHeadFX.mHead.getChildren().add(rightEyeGroup);
-		else
-			mHeadFX.mHead.getChildren().set(5, rightEyeGroup);
 	}
 
 	public void update()
@@ -390,7 +364,7 @@ public class RightEyeFX extends BodyPartFX {
 			mColorRecorder = mColor;
 	}
 	
-	private Path createEllipsePath(double centerX, double centerY, double radiusX, double radiusY, double rotate, Color eyeColor, Color borderColor) {
+	private Path createEllipsePath(Path startPath, double centerX, double centerY, double radiusX, double radiusY, double rotate, Color eyeColor, Color borderColor) {
         ArcTo arcTo = new ArcTo();
         arcTo.setX(centerX - radiusX + 1); // to simulate a full 360 degree celcius circle.
         arcTo.setY(centerY - radiusY);
@@ -400,18 +374,29 @@ public class RightEyeFX extends BodyPartFX {
         arcTo.setRadiusY(radiusY);
         arcTo.setXAxisRotation(rotate);
         
-        Path path = new Path();
-        path.getElements().add(new MoveTo(centerX - radiusX, centerY - radiusY));
-        path.getElements().add(arcTo);
-        path.getElements().add(new ClosePath());
+        startPath.getElements().add(new MoveTo(centerX - radiusX, centerY - radiusY));
+        startPath.getElements().add(arcTo);
+        startPath.getElements().add(new ClosePath());
 
         if(borderColor != null)
         {
-        	path.setStroke(Color.BLACK);
-            path.setStrokeWidth(1);
+        	startPath.setStroke(Color.BLACK);
+        	startPath.setStrokeWidth(1);
         }
         
-        path.setFill(eyeColor);
-        return path;
+        startPath.setFill(eyeColor);
+        return startPath;
     }
+	
+	private Path createBorder(Path startBorder)
+	{
+		startBorder.getElements().add(new MoveTo(mStart.x, mStart.y));
+		startBorder.getElements().add(new QuadCurveTo(mStart.x - 10, mStart.y - 13, mStart.x - 20, mStart.y));
+		startBorder.getElements().add(new QuadCurveTo(mStart.x - 10, mStart.y + 13, mStart.x, mStart.y));
+		startBorder.setStrokeWidth(1);
+		startBorder.setStroke(Color.BLACK);
+		startBorder.setFill(Color.WHITE);
+		
+		return startBorder;
+	}
 }
