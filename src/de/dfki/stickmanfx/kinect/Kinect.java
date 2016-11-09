44cd3dd52151bdd6dcb348ada918108b5f19d5a8
Beka Aptsiauri <bekatiflis@gmail.com>
2016-11-10 00:18:51 +0100

    Emo Parallel

--- src/de/dfki/stickmanfx/kinect/Kinect.java
+++ src/de/dfki/stickmanfx/kinect/Kinect.java
@@ -23,17 +23,8 @@ public class Kinect extends AnimationFX
       Skeleton skeleton = Skeleton.getSkeleton( skeletonId, skeletonTracked, positions,
                                                 orientations, jointStatus, this );
  
-//      BOX.SETROTATE( (INT) SKELETON.GETBODYORIENTATION() );  // REDUCE "NOISE" WITH (INT)
-//      TEXT.SETTEXT( "" + (INT) SKELETON.GETBODYORIENTATION() );
-//      TExt.setRotate( (int) skeleton.getBodyOrientation() );
-//      currentStickman.mUpperBody.setRotationAxis(Rotate.Y_AXIS);
-//      currentStickman.mUpperBody.setRotate((int)skeleton.getBodyOrientation());
-      System.out.println((int) skeleton.getBodyOrientation());
       currentStickman.mUpperBody.mYRotation = (int) skeleton.getBodyOrientation();
       currentStickman.mUpperBody.calculate(0);
-//      mAnimationPartFX = new ArrayList<>();
-//      mAnimationPartFX.add(new AnimationContentFX(currentStickman.mUpperBody, "yrotate", (int) skeleton.getBodyOrientation()));
-//      playAnimationPart(5);
     }
      
     @Override
