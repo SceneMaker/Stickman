0efea698280ecb928086e07d9d6e22d46cca6f9c
Beka <bekatiflis@gmail.com>
2017-06-04 19:07:31 +0200

    delete unused methods and members

--- src/de/dfki/reeti/body/BodyPart.java
+++ src/de/dfki/reeti/body/BodyPart.java
@@ -6,20 +6,10 @@
 package de.dfki.reeti.body;
 
 import de.dfki.reeti.animationlogic.AnimatorReeti;
-import javafx.animation.FadeTransition;
-import javafx.animation.Interpolator;
-import javafx.animation.PathTransition;
-import javafx.animation.Timeline;
 import javafx.application.Platform;
-import javafx.scene.Node;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
-import javafx.scene.shape.*;
-import javafx.scene.shape.Shape;
-import javafx.scene.transform.Affine;
-import javafx.util.Duration;
-
 import java.awt.*;
 import java.net.URL;
 
@@ -40,17 +30,14 @@ public abstract class BodyPart extends Pane
 
     public int mShapeAnimationStep = 0;
 
-    public int mDefaultTranslation = 0;
-    public double mXTranslation = mDefaultTranslation;
-    public double mYTranslation = mDefaultTranslation;
-    public double mZTranslation = mDefaultTranslation;
-    public double mXToTranslation = mDefaultTranslation;
-    public double mYToTranslation = mDefaultTranslation;
-    public double mZToTranslation = mDefaultTranslation;
+    private int mDefaultTranslation = 0;
+    private double mXTranslation = mDefaultTranslation;
+    private double mYTranslation = mDefaultTranslation;
+    private double mZTranslation = mDefaultTranslation;
 
-    public double mXTranslationStep = 0.0f;
-    public double mYTranslationStep = 0.0f;
-    public double mZTranslationStep = 0.0f;
+    private double mXTranslationStep = 0.0f;
+    private double mYTranslationStep = 0.0f;
+    private double mZTranslationStep = 0.0f;
 
     public int mDefaultRotation = 0;
     public Point mDefaultRotationPoint = new Point(0, 0);
@@ -59,48 +46,33 @@ public abstract class BodyPart extends Pane
     public double mYRotation = mDefaultRotation;
     public double mZRotation = mDefaultRotation;
     public double mToDegreeX = mDefaultRotation;
-    public double mToDegreeY = mDefaultRotation;
-    public double mToDegreeZ = mDefaultRotation;
-    public double mXRotationStep = 0.0f;
-    public double mYRotationStep = 0.0f;
-    public double mZRotationStep = 0.0f;
+    private double mXRotationStep = 0.0f;
+    private double mYRotationStep = 0.0f;
+    private double mZRotationStep = 0.0f;
 
     public Color mColor = Color.rgb(0, 0, 0);
-    public Color mColorRecorder;
-    public double mXRotatationRecorder;
-    public double mYRotatationRecorder;
-    public double mZRotatationRecorder;
 
     private static PhongMaterial material = null;
-
-    public BasicStroke mStroke = new BasicStroke(3.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
 
     public void init()
     {
         this.setPrefHeight(mSize.height);
         this.setPrefWidth(mSize.width);
-        mColorRecorder = mColor;
-        mXRotatationRecorder = mXRotation;
-        mYRotatationRecorder = mYRotation;
-        mZRotatationRecorder = mZRotation;
         calculate(0);
     }
 
     public void set_X_Translation(int length)
     {
-        mXToTranslation = mXTranslation + length;
         mXTranslationStep = (double) length / AnimatorReeti.sMAX_ANIM_STEPS;
     }
 
     public void set_Y_Translation(int length)
     {
-        mYToTranslation = mYTranslation + length;
         mYTranslationStep = (double) length / AnimatorReeti.sMAX_ANIM_STEPS;
     }
 
     public void set_Z_Translation(int length)
     {
-        mZToTranslation = mZTranslation + length;
         mZTranslationStep = (double) length / AnimatorReeti.sMAX_ANIM_STEPS;
     }
 
@@ -143,8 +115,6 @@ public abstract class BodyPart extends Pane
         mZRotation = mDefaultRotation;
 
         mToDegreeX = mDefaultRotation;
-        mToDegreeY = mDefaultRotation;
-        mToDegreeZ = mDefaultRotation;
         mXRotationStep = 0.0f;
     }
 
@@ -156,13 +126,11 @@ public abstract class BodyPart extends Pane
 
     public void set_Y_Rotation(int degree)
     {
-        mToDegreeY = mYRotation + degree;
         mYRotationStep = (double) degree / AnimatorReeti.sMAX_ANIM_STEPS;
     }
 
     public void set_Z_Rotation(int degree)
     {
-        mToDegreeZ = mZRotation + degree;
         mZRotationStep = (double) degree / AnimatorReeti.sMAX_ANIM_STEPS;
     }
 
@@ -253,70 +221,6 @@ public abstract class BodyPart extends Pane
     public synchronized void calculate(int step)
     {
         createShape();
-    }
-
-    public void update()
-    {
-        recordColor();
-    }
-
-    protected void recordColor()
-    {
-
-    }
-
-    public void showHearts(Head mHeadFX, double xMovement, double yMovement1, double yMovement2)
-    {
-        int numHearts = 7;
-
-        Ellipse path = new Ellipse(mHeadFX.getHalfWidth() + 4, mHeadFX.getHalfHeight() - 50, 60, 20);
-
-        for (int i = 0; i < numHearts; i++)
-        {
-            Path heart = new Path();
-            heart.getElements().add(new MoveTo(mStart.x - 10, mStart.y));
-            heart.getElements().add(new QuadCurveTo(mStart.x - 10 - xMovement - 5, mEnd.y - yMovement2, mStart.x - 10,
-                    mEnd.y + yMovement1 + 15));
-            heart.getElements().add(new MoveTo(mStart.x - 10, mStart.y));
-            heart.getElements().add(new QuadCurveTo(mStart.x - 10 + xMovement + 5, mEnd.y - yMovement2, mStart.x - 10,
-                    mEnd.y + yMovement1 + 15));
-            heart.setFill(Color.RED);
-
-            this.getChildren().addAll(heart);
-
-            FadeTransition ft = new FadeTransition(Duration.millis(200), heart);
-            ft.setFromValue(1.0);
-            ft.setToValue(0.1);
-            ft.setCycleCount(Timeline.INDEFINITE);
-            ft.setAutoReverse(true);
-            ft.play();
-
-            PathTransition transition = createPathTransition(path, heart);
-            transition.jumpTo(Duration.seconds(10).multiply(i * 1.0 / numHearts));
-            this.toFront();
-            transition.play();
-        }
-
-    }
-
-    private PathTransition createPathTransition(Shape shape, Node node)
-    {
-        final PathTransition transition = new PathTransition(Duration.seconds(10), shape, node);
-
-        transition.setAutoReverse(false);
-        transition.setCycleCount(PathTransition.INDEFINITE);
-        transition.setInterpolator(Interpolator.LINEAR);
-
-        return transition;
-    }
-
-    public void rotatePerlinNoise(double mWobble, int x, int y)
-    {
-        Affine af = new Affine();
-        // Out put perlin noise
-        af.appendRotation(Math.toRadians(mWobble), x, y);
-        this.getTransforms().clear();
-        this.getTransforms().add(af);
     }
 
     protected PhongMaterial getMaterial()
